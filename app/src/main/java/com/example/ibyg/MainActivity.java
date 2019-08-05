3394f32892789d041c3a84345e0fec6281223fc6
kdh <daekim95@naver.com>
2019-08-05 15:52:23 +0900

    업주 메인화면+카페등록01

--- app/src/main/java/com/example/ibyg/MainActivity.java
+++ app/src/main/java/com/example/ibyg/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.ibyg;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();  //제목줄 객체 얻어오기
+        actionBar.setTitle("알고가");  //액션바 제목설정
+
+        actionBar.setDisplayHomeAsUpEnabled(true);   //업버튼 <- 만들기
+
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CafeManager.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
