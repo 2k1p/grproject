bef207ab4732e8520f1e13647041a0c3b9036e1d
kdh <daekim95@naver.com>
2019-08-16 17:36:14 +0900

    업주 메인화면+카페등록04+공지사항03(등록하기DB)

--- app/src/main/java/com/example/ibyg/NoticeAdd.java
+++ app/src/main/java/com/example/ibyg/NoticeAdd.java
@@ -1,14 +1,24 @@
 package com.example.ibyg;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class NoticeAdd extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class NoticeAdd extends MainActivity {
+    private static final String TAG = "NoticeAdd";
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +31,61 @@ public class NoticeAdd extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);   //뒤로가기버튼 <- 만들기
 
 
-        Button button = findViewById(R.id.noticeadd);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {    //카페관리 눌렀을때 다음화면이동
-                Intent intent = new Intent(NoticeAdd.this, NoticeManagement.class);
 
-                startActivity(intent);
-            }
-        });
+        findViewById(R.id.noticeadd).setOnClickListener(onClickListener);
+
+
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.noticeadd:    //등록하기 버튼
+                    myStartActivity(NoticeManagement.class);
+                    profileUpdate();
+                    break;
+            }
+        }
+    };
+
+
+    //안드로이드 + 파이어베이스 SNS앱 만들기 part10 [게시글 작성 화면 구현]
+    private void profileUpdate(){
+        String title = ((EditText)findViewById(R.id.titleEditText)).getText().toString();
+        String contents = ((EditText)findViewById(R.id.contentEditText)).getText().toString();
+
+        if(title.length() > 0 && contents.length() > 0){
+            user = FirebaseAuth.getInstance().getCurrentUser();
+            OwnerNoticeInfo ownerNoticeInfo = new OwnerNoticeInfo(title, contents, user.getUid());
+            uploader(ownerNoticeInfo);
+
+        }else{
+            startToast("회원정보를 입력해주세요.");
+        }
+    }
+
+    private void uploader(OwnerNoticeInfo ownerNoticeInfo){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("owner_notice").add(ownerNoticeInfo)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot written with ID: "+ documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+    private void startToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+
+
 }
