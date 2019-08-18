a72ca1b37b983881964715b0996d07af344205df
dbswo <dbswoqhrtk@gmail.com>
2019-08-18 20:27:08 +0900

    위기를 넘긴 대현이와 나

--- app/src/main/java/com/example/ibyg/LoginActivity.java
+++ app/src/main/java/com/example/ibyg/LoginActivity.java
@@ -2,7 +2,7 @@ package com.example.ibyg;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
