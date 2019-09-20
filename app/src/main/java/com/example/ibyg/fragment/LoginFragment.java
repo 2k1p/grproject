e5cf27f6760880170503cb30dbf86fb9c5a0c08d
kdh <daekim95@naver.com>
2019-09-20 15:16:25 +0900

    하단 메뉴바 성공

--- app/src/main/java/com/example/ibyg/fragment/LoginFragment.java
+++ app/src/main/java/com/example/ibyg/fragment/LoginFragment.java
@@ -2,31 +2,52 @@ package com.example.ibyg.fragment;
 
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.example.ibyg.BasicActivity;
 import com.example.ibyg.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
-public class LoginFragment extends Fragment {
-
-
-    public LoginFragment() {
-        // Required empty public constructor
-    }
-
+//로그인화면, 구글로그인
+public class LoginFragment extends BasicActivity {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_login);
+
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+        bottomNavigationView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(1);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ic_android:
+                        myStartActivity(ListingActivity.class);
+                        break;
+
+                    case R.id.ic_books:
+
+                        break;
+
+
+                }
+
+
+                return false;
+            }
+        });
     }
+
 
 }
